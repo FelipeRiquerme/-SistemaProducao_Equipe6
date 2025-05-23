0c85aaf7bd334317c1f958c6eea40c0815a8d980
DiguinhoDebug <rodrigo_ls_ribeiro@estudante.sesisenai.org.br>
2025-05-22 22:13:21 -0300

    Update teste.java

--- JavaCod/teste.java
+++ JavaCod/teste.java
@@ -1,42 +1,45 @@
-package teste1;
+package Teste;
+
 import java.util.*;
 
-public class Menu {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+public class Teste3 {
+Scanner entrada = new Scanner (System.in );
+	public ArrayList <String> Maquinas = new ArrayList<>();
+	public ArrayList <String> Setor = new ArrayList<>();
+	public int[][] smn1 = new int[8][3];
+	int dia = 1;
+	
+	public void cadastro() {
+		System.out.println("Digite o nome da máquina: ");
+		String mqn = entrada.nextLine();
+		Maquinas.add(mqn);
+		System.out.println("Digite o setor");
+		String set = entrada.nextLine();
+		Setor.add(set);
+		System.out.println("Cadastrado com sucesso!");
+	}
+	public void mostrarMqnSet() {
+		System.out.print("Maquinas: ");
+		System.out.println("  " + Maquinas);
+		System.out.println("Setores: " + Setor);
+	}
+	public void regispdc() {
+		dia++;
+		for(int i = 0; i < smn1.length; i++) {
+		System.out.println("Digite a produção do dia " + i);
+		smn1[dia][i] = entrada.nextInt();
+	}
 
-        // MENU
-        System.out.println("---- MENU ----");
-        System.out.println("1 - Cadastrar Máquina");
-        System.out.println("2 - Cadastrar Setor");
-        System.out.println("3 - Cadastrar Produtos");
-        System.out.println("4 - Listar Cadastros");
-        System.out.println("5 - SAIR");
-
-        int opcao = entrada.nextInt();
-        entrada.nextLine(); 
-
-        switch (opcao) {
-            case 1:
-                System.out.println("Cadastrando máquina");
-                break;
-            case 2:
-                System.out.println("Cadastrando setor");
-                break;
-            case 3:
-                System.out.println("Cadastrando produtos");
-                break;
-            case 4:
-                System.out.println("Listando cadastros");
-                break;
-            case 5:
-                System.out.println("Você saiu");
-                break;
-            default:
-                System.out.println("Opção inválida");
-                break;
-        }
-
-        entrada.close(); 
-    }
-}
+	}
+	public void relatorio() {
+		for(int i = 0; i < smn1.length;i++) {
+			for(int j = 0; i < smn1.length;i++) {
+				System.out.println(smn1);
+			}
+		}
+	}
+	public void geral() {
+		
+	}
+	}
+	
