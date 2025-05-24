70ba653b97356b5f23bb80324c1b47b60314ce85
DiguinhoDebug <rodrigo_ls_ribeiro@estudante.sesisenai.org.br>
2025-05-23 22:13:35 -0300

    Update menu.java
    -- Menu do usuário atualizado
    --Switch case otimizado

--- JavaCod/menu.java
+++ JavaCod/menu.java
@@ -1,17 +1,15 @@
-package Teste;
+package teste;
 import java.util.*;
 public class Menu {
 	public static void main(String[] args){
-		Teste3 classe = new Teste3();
+		Teste3 Classe = new Teste3();
 		Scanner  entrada = new Scanner(System.in);
 		int opcao =0;
-		//MENU
 		do {
-			System.out.println(classe.smn1);
 		System.out.println("\n ---- MENU ---- ");
 		System.out.println("1- Cadastrar Setor e Maquina: ");
-		System.out.println("2- Cadastrar Produtos: " );
-		System.out.println("3- Gerar relatório semanal: ");
+		System.out.println("2- Cadastrar Produção: " );
+		System.out.println("3- Mostrar Setores e Maquinas cadastrados: ");
 		System.out.println("4- Gerar média: ");
 		System.out.println("5- SAIR ");
 		System.out.println("Escolha uma opção: ");
@@ -19,26 +17,18 @@ public class Menu {
 		 opcao = entrada.nextInt();
 		 entrada.nextLine();
 		 
-		
-	
 		switch(opcao) {
+		case 1: Classe.cadastro(); break;
 		
-		case 1:
-			//classe.cadastro();
-		break;
-		case 2:	
-			classe.regispdc();
-		break;
-		case 3:
-		System.out.println("Gerando relatório....");
-		break;
-		case 4:
-		System.out.println("listando cadastros");	break;
-		case 5:
-		System.out.println("Voce saiu ");	break;
-		default:
-		System.out.println("Escolha uma Opção correta "); break;
+		case 2:	Classe.regispdc(entrada, 3); break;
 		
+		case 3: Classe.mostrarMqnSet(); break;
+		
+		case 4: Classe.imprimirSmns();	break;
+		
+		case 5: System.out.println("Voce saiu "); break;
+		
+		default: System.out.println("Escolha uma Opção correta "); break;
 		}
 	}while(opcao != 5);
 		entrada.close();
