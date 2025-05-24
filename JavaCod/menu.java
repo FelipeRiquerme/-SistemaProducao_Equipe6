03d124eeed30056ce32cb4ccdd7e1b0ef25b8106
DiguinhoDebug <rodrigo_ls_ribeiro@estudante.sesisenai.org.br>
2025-05-24 16:37:49 -0300

    Update menu.java

--- JavaCod/menu.java
+++ JavaCod/menu.java
@@ -10,8 +10,10 @@ public class Menu {
 		System.out.println("1- Cadastrar Setor e Maquina: ");
 		System.out.println("2- Cadastrar Produção: " );
 		System.out.println("3- Mostrar Setores e Maquinas cadastrados: ");
-		System.out.println("4- Gerar média: ");
-		System.out.println("5- SAIR ");
+		System.out.println("4- Gerar relatório ");
+		System.out.println("5- Gerar média: ");
+		System.out.println("6- Buscar por setor: ");
+		System.out.println("7- SAIR ");
 		System.out.println("Escolha uma opção: ");
 		
 		 opcao = entrada.nextInt();
@@ -20,17 +22,21 @@ public class Menu {
 		switch(opcao) {
 		case 1: Classe.cadastro(); break;
 		
-		case 2:	Classe.regispdc(entrada, 3); break;
+		case 2:	Classe.regispdc(entrada); break;
 		
 		case 3: Classe.mostrarMqnSet(); break;
 		
 		case 4: Classe.imprimirSmns();	break;
 		
-		case 5: System.out.println("Voce saiu "); break;
+		case 5: Classe.geral();	break;
+		
+		case 6: Classe.buscarSetor(); break;
+		
+		case 7: System.out.println("Voce saiu "); System.exit(1);
 		
 		default: System.out.println("Escolha uma Opção correta "); break;
 		}
-	}while(opcao != 5);
+	}while(opcao != 7);
 		entrada.close();
 	}
 }
